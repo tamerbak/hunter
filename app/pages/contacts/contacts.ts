3754f273ad699cec8dcfc1cdbcb0127ed5724828
Jakjoud Abdeslam <jakjoud@gmail.com>
2016-06-08 13:38:53 +0200

    Bonuses for creating a jobyer

--- app/pages/contacts/contacts.ts
+++ app/pages/contacts/contacts.ts
@@ -1,12 +1,13 @@
-import {Page, NavController, NavParams, LocalStorage, Storage, Modal} from 'ionic-angular';
+import {Page, NavController, NavParams, LocalStorage, Storage, Modal, SqlStorage, Alert} from 'ionic-angular';
 import {OpportunitiesService} from "../../providers/opportunities-service/opportunities-service";
 import {ModalNewCandidatePage} from "../modal-new-candidate/modal-new-candidate";
 import {SMS} from "ionic-native/dist/index";
 import {Component} from "@angular/core";
+import {NotationService} from "../../providers/notation-service/notation-service";
 
 @Component({
     templateUrl: 'build/pages/contacts/contacts.html',
-    providers:[OpportunitiesService]
+    providers:[OpportunitiesService, NotationService]
 })
 export class ContactsPage {
     opportunity:any;
@@ -14,11 +15,17 @@ export class ContactsPage {
     candidates : any [];
     voidCandidates: boolean = true;
     storage : any;
+    sqlStore : any;
+    account : any;
+    notationService : NotationService;
 
     constructor(public nav: NavController,
                 public navParams : NavParams,
+                notationService : NotationService,
                 opportunitiesService : OpportunitiesService) {
         this.storage = new Storage(LocalStorage);
+        this.sqlStore = new Storage(SqlStorage);
+        this.notationService = notationService;
         this.opportunitiesService = opportunitiesService;
         this.storage.get('OPPORTUNITY').then(opp => {
             this.opportunity = JSON.parse(opp);
@@ -27,13 +34,16 @@ export class ContactsPage {
                 this.candidates = data;
             });
         });
-
+        this.sqlStore.get('currentUser').then(data =>{
+            this.account = JSON.parse(data);
+        });
     }
 
     addNewCandidates(){
         let newCandidatePage = new Modal(ModalNewCandidatePage);
         newCandidatePage.onDismiss((candidate) =>{
             if(candidate && candidate.id>0){
+                this.notationService.notationJobyer(this.account.id);
                 this.voidCandidates = false;
                 this.candidates.push(candidate);
                 if(candidate.createdAccount){
@@ -47,6 +57,12 @@ export class ContactsPage {
                     console.log(message);
                     SMS.send(candidate.tel, message, options);
                 }
+                let alert = Alert.create({
+                    title: 'Invitation envoyée',
+                    subTitle: 'Une invitation a été adressé à votre contact',
+                    buttons: ['OK']
+                });
+                this.nav.present(alert);
             }
         });
         this.storage.set('OPP',JSON.stringify(this.opportunity)).then(()=>{
