dc817a79d060007f76a15087b51b48005fa52ca9
amalrochd <amalrochd@gmail.com>
2016-06-09 11:35:29 +0000

    update phone connection

--- app/pages/phone/phone.ts
+++ app/pages/phone/phone.ts
@@ -12,10 +12,10 @@ import {Storage, SqlStorage} from 'ionic-angular';
 import {Component} from '@angular/core';
 
 /**
- * @author Amal ROCHD
- * @description authentication by phone view
- * @module Authentication
- */
+* @author Amal ROCHD
+* @description authentication by phone view
+* @module Authentication
+*/
 @Component({
 	templateUrl: 'build/pages/phone/phone.html',
 	providers: [AuthenticationService, LoadListService, DataProviderService, GlobalService, ValidationDataService]
@@ -35,36 +35,37 @@ export class PhonePage {
 	password1: string;
 	password2: string;
 	storage:any;
-
+	isPhoneNumValid = true;
+	
 	/**
-	 * @description While constructing the view, we load the list of countries to display their codes
-	 */
+		* @description While constructing the view, we load the list of countries to display their codes
+	*/
 	constructor(public nav: NavController,
-				public gc: GlobalConfigs, private authService: AuthenticationService, private loadListService: LoadListService, private dataProviderService: DataProviderService, private globalService: GlobalService, private validationDataService: ValidationDataService, public events: Events) {
+	public gc: GlobalConfigs, private authService: AuthenticationService, private loadListService: LoadListService, private dataProviderService: DataProviderService, private globalService: GlobalService, private validationDataService: ValidationDataService, public events: Events) {
 		// Set global configs
 		// Get target to determine configs
 		this.projectTarget = gc.getProjectTarget();
 		this.storage = new Storage(SqlStorage);
-
+		
 		// get config of selected target
 		let config = Configs.setConfigs(this.projectTarget);
-
+		
 		// Set local variables and messages
 		this.themeColor = config.themeColor;
 		this.isEmployer = (this.projectTarget == 'employer');
 		this.phoneTitle = "Téléphone";
 		this.index = 33;
 		this.libelleButton = "Se connecter";
-
+		
 		//load countries list
 		this.loadListService.loadCountries(this.projectTarget).then((data) => {
 			this.pays = data.data;
 		});
 	}
-
+	
 	/**
-	 * @description Display the list of countries in an alert
-	 */
+		* @description Display the list of countries in an alert
+	*/
 	doRadioAlert() {
 		let alert = Alert.create();
 		alert.setTitle('Choisissez votre pays');
@@ -73,7 +74,7 @@ export class PhonePage {
 				type: 'radio',
 				label: p.nom,
 				value: p.indicatif_telephonique,
-
+				
 				//france code by default checked
 				checked: p.indicatif_telephonique == '33'
 			});
@@ -86,14 +87,14 @@ export class PhonePage {
 				this.index = data;
 			}
 		});
-
+		
 		this.nav.present(alert).then(() => {
 		});
 	}
-
+	
 	/**
-	 * @description function called to authenticate a user
-	 */
+		* @description function called to authenticate a user
+	*/
 	authenticate() {
 		var indPhone = this.index + this.phone;
 		let loading = Loading.create({
@@ -105,126 +106,132 @@ export class PhonePage {
 			spinner : 'hide'
 		});
 		this.nav.present(loading);
-
-        //call the service of autentication
+		
+		//call the service of autentication
 		this.authService.authenticate(this.email, indPhone, this.password1, this.projectTarget)
-			.then(data => {
+		.then(data => {
+			console.log(data);
+			//case of authentication failure : server unavailable or connection probleme 
+			if (!data || data.length == 0 || (data.id == 0 && data.status == "failure")) {
 				console.log(data);
-				//case of authentication failure : server unavailable or connection probleme 
-				if (!data || data.length == 0 || (data.id == 0 && data.status == "failure")) {
-					console.log(data);
-					loading.dismiss();
-					this.globalService.showAlertValidation("VitOnJob", "Serveur non disponible ou problème de connexion.");
-					return;
+				loading.dismiss();
+				this.globalService.showAlertValidation("VitOnJob", "Serveur non disponible ou problème de connexion.");
+				return;
+			}
+			//case of authentication failure : incorrect password 
+			if (data.id == 0 && data.status == "passwordError") {
+				console.log("Password error");
+				loading.dismiss();
+				if(!this.showEmailField){
+					this.globalService.showAlertValidation("VitOnJob", "Votre mot de passe est incorrect.");
+					}else{
+					console.log("used email error");
+					this.globalService.showAlertValidation("VitOnJob", "Cette adresse email a été déjà utilisé. Veuillez choisir une autre.");
 				}
-				//case of authentication failure : incorrect password 
-				if (data.id == 0 && data.status == "passwordError") {
-					console.log("Password error");
-					loading.dismiss();
-					this.globalService.showAlertValidation("VitOnJob", "Votre mot de passe est incorrect");
-					return;
-				}
-
-				//case of authentication success
-				this.authService.setObj('connexion', null);
-				this.authService.setObj('currentUser', null);
-				var connexion = {
-					'etat': true,
-					'libelle': 'Se déconnecter',
-					'employeID' : (this.projectTarget == 'jobyer' ? data.jobyerId : data.employerId)
-				};
-
-				//load device token to current account
-				var token;
-				this.authService.getObj('deviceToken').then(val => {
-					token = val;
-				});
-				var accountId = data.id;
-				if (token) {
-					console.log("insertion du token : " + token);
-					this.authService.insertToken(token, accountId, this.projectTarget);
-				}
-
-				this.storage.set('connexion', JSON.stringify(connexion));
-				this.storage.set('currentUser', JSON.stringify(data)).then(()=>{
-                    this.events.publish('user:login');
-
-                    //user is connected, then change the name of connexion btn to deconnection
-                    this.gc.setCnxBtnName("Déconnexion");
-                    loading.dismiss();
-
-                    //if user is connected for the first time, redirect him to the page 'civility', else redirect him to the home page
-                    var isNewUser = data.newAccount;
-                    if (isNewUser) {
-                        this.globalService.showAlertValidation("VitOnJob", "Bienvenue dans votre espace VitOnJob!");
-                        this.nav.push(InfoUserPage, {
-                            currentUser: data});
-                    } else {
-                        this.nav.rootNav.setRoot(HomePage);
-                    }
-                });
-
+				return;
+			}
+			
+			//case of authentication success
+			this.authService.setObj('connexion', null);
+			this.authService.setObj('currentUser', null);
+			var connexion = {
+				'etat': true,
+				'libelle': 'Se déconnecter',
+				'employeID' : (this.projectTarget == 'jobyer' ? data.jobyerId : data.employerId)
+			};
+			
+			//load device token to current account
+			var token;
+			this.authService.getObj('deviceToken').then(val => {
+				token = val;
 			});
+			var accountId = data.id;
+			if (token) {
+				console.log("insertion du token : " + token);
+				this.authService.insertToken(token, accountId, this.projectTarget);
+			}
+			
+			this.storage.set('connexion', JSON.stringify(connexion));
+			this.storage.set('currentUser', JSON.stringify(data)).then(()=>{
+				this.events.publish('user:login');
+				
+				//user is connected, then change the name of connexion btn to deconnection
+				this.gc.setCnxBtnName("Déconnexion");
+				loading.dismiss();
+				
+				//if user is connected for the first time, redirect him to the page 'civility', else redirect him to the home page
+				var isNewUser = data.newAccount;
+				if (isNewUser) {
+					this.globalService.showAlertValidation("VitOnJob", "Bienvenue dans votre espace VitOnJob!");
+					this.nav.push(InfoUserPage, {
+					currentUser: data});
+					} else {
+					this.nav.rootNav.setRoot(HomePage);
+				}
+			});
+			
+		});
 	}
-
+	
 	/**
-	 * @description function called to decide if the auth/inscr button should be disabled
-	 */
+		* @description function called to decide if the auth/inscr button should be disabled
+	*/
 	isAuthDisabled() {
 		if (this.showEmailField == true) {
 			//inscription
-			return (!this.index || !this.phone || this.showPhoneError() || !this.password1 || this.showPassword1Error() || !this.password2 || this.showPassword2Error() || !this.email || this.showEmailError())
-		} else {
+			return (!this.index || !this.phone || !this.isPhoneNumValid || !this.password1 || this.showPassword1Error() || !this.password2 || this.showPassword2Error() || !this.email || this.showEmailError())
+			} else {
 			//connection
-			return (!this.index || !this.phone || !this.password1)
+			return (!this.index || !this.phone || !this.isPhoneNumValid || !this.password1 || this.showPassword1Error())
 		}
 	}
-
+	
 	/**
-	 * @description function called on change of the phone input to validate it
-	 */
+		* @description function called on change of the phone input to validate it
+	*/
 	checkForString(e){
 		if (e.keyCode < 48 || e.keyCode > 57){
 			e.preventDefault();
 			return;
 		}
 	}
-
+	
 	/**
-	 * @description validate phone data field and call the function that search for it in the server
-	 */
+		* @description validate phone data field and call the function that search for it in the server
+	*/
 	watchPhone(e, el) {
 		if (this.phone) {
-			this.phone = this.phone.replace("-", "").replace(".", "").replace("+", "").replace(" ", "").replace("(", "").replace(")", "").replace("/", "").replace(",", "").replace("#", "").replace("*", "").replace(";", "").replace("N", "");
-
-			if (this.phone.length == 10) {
-				if (this.phone.substring(0, 1) == '0') {
-					this.phone = this.phone.substring(1, 10);
-				} else {
-					this.phone = this.phone.substring(0, 9);
-				}
+			this.isPhoneNumValid = false;
+			if (e.target.value.substring(0,1) == '0') {
+				e.target.value = e.target.value.substring(1, e.target.value.length);
 			}
-			if (this.phone.length == 9) {
-				this.isRegistration(el);
+			if (e.target.value.includes('.')) {
+				e.target.value = e.target.value.replace('.', '');
+			}
+			if(e.target.value.length > 9){
+				e.target.value = e.target.value.substring(0, 9);
+			}
+			if (e.target.value.length == 9) {
+				this.isRegistration(e.target.value);
+				this.isPhoneNumValid = true;
 			}
 		}
 	}
-
+	
 	/**
-	 * @description show error msg if phone is not valid
-	 */
+		* @description show error msg if phone is not valid
+	*/
 	showPhoneError(){
-		if(this.phone)
-			return (this.phone.length != 9);
+		return !this.isPhoneNumValid;
 	}
-
+	
 	/**
-	 * @description function called when the phone input is valid to decide if the form is for inscription or authentication
-	 */
-	isRegistration(el) {
-		if (this.isPhoneValid()) {
-			// Testing if phone number exists
-			var tel = "+" + this.index + this.phone;
+		* @description function called when the phone input is valid to decide if the form is for inscription or authentication
+	*/
+	isRegistration(phone) {
+		if (this.isPhoneValid(phone)) {
+			//On teste si le tél existe dans la base
+			var tel = "+" + this.index + phone;
 			this.dataProviderService.getUserByPhone(tel, this.projectTarget).then((data) => {
 				if (!data || data.status == "failure") {
 					console.log(data);
@@ -235,68 +242,69 @@ export class PhonePage {
 					this.showEmailField = true;
 					this.email = "";
 					this.libelleButton = "S'inscrire";
-				} else {
+					} else {
 					this.email = data.data[0]["email"];
 					this.libelleButton = "Se connecter";
 					this.showEmailField = false;
 				}
-			})
-		} else {
+			});
+			} else {
+			//ça sera toujours une connexion
 			this.showEmailField = true;
 			this.libelleButton = "S'inscrire";
 			this.email = "";
 		}
 	}
-
+	
 	/**
-	 * @description validate the phone format
-	 */
-	isPhoneValid() {
-		if (this.phone != undefined) {
+		* @description validate the phone format
+	*/
+	isPhoneValid(tel) {
+		if (this.phone) {
 			var phone_REGEXP = /^0/;
-			var isMatchRegex = phone_REGEXP.test(this.phone);
-			console.log("isMatchRegex = " + isMatchRegex);
-			if (Number(this.phone.length) >= 9 && !isMatchRegex) {
-				console.log('test phone');
+			//check if the phone number start with a zero
+			var isMatchRegex = phone_REGEXP.test(tel);
+			if (Number(tel.length) == 9 && !isMatchRegex) {
+				console.log('phone number is valid');
 				return true;
 			}
 			else
-				return false;
-		} else
 			return false;
+		} else
+		return false;
 	}
-
+	
 	/**
-	 * @description validate the email format
-	 */
+		* @description validate the email format
+	*/
 	showEmailError() {
 		if(this.email)
-			return !(this.validationDataService.checkEmail(this.email));
+		return !(this.validationDataService.checkEmail(this.email));
 		else
-			return false
+		return false
 	}
-
+	
 	/**
-	 * @description show error msg if password is not valid
-	 */
+		* @description show error msg if password is not valid
+	*/
 	showPassword1Error(){
-		if(this.password1 && this.showEmailField)
-			return this.password1.length < 6;
+		if(this.password1)
+		return this.password1.length < 6;
 	}
-
+	
 	/**
-	 * @description check if the password and its confirmation are the same
-	 */
+		* @description check if the password and its confirmation are the same 
+	*/
 	showPassword2Error(){
 		if(this.password2)
-			return this.password2 != this.password1;
+		return this.password2 != this.password1;
 	}
-
-
-
+	
+	
+	
 	/**
-	 * @description return to the home page
-	 */
+		* @description return to the home page
+	*/
 	goBack() {
 		this.nav.rootNav.setRoot(HomePage)
 	}
